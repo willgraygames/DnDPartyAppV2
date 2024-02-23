5d91e5bd15fa763f9669de6e67216e10afb518f5
Will Gray <willgraygames@gmail.com>
2024-02-23 16:22:18 -0500

    Initial Commit

---
+++ dnd-party-app/src/server/src/main/java/com/willgraygames/dndpartyapp/DndPartyAppApplication.java
@@ -0,0 +1,13 @@
+package com.willgraygames.dndpartyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndPartyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndPartyAppApplication.class, args);
+	}
+
+}
---
+++ dnd-party-app/src/server/src/test/java/com/willgraygames/dndpartyapp/DndPartyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willgraygames.dndpartyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndPartyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
